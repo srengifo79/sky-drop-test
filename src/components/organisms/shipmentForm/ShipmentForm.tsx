3d286a928bcd3c8a13420ab7f89818710c79a8e8
srengifo79 <s.rengifo79@gmail.com>
2021-09-07 13:19:48 -0500

    Shipment form styles

--- src/components/organisms/shipmentForm/ShipmentForm.tsx
+++ src/components/organisms/shipmentForm/ShipmentForm.tsx
@@ -4,6 +4,34 @@ import { useForm } from "react-hook-form";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      backgroundColor: theme.palette.primary.main,
+      padding: "4% 6%",
+    },
+    title: {
+      color: theme.palette.primary.contrastText,
+    },
+    textField: {
+      "& .MuiInputBase-root": {
+        color: theme.palette.primary.contrastText,
+      },
+      "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+        borderColor: theme.palette.common.white,
+      },
+      "& .MuiFormLabel-root": {
+        color: "white",
+      },
+    },
+    button: {
+      backgroundColor: theme.palette.secondary.main,
+    },
+  })
+);
 
 export type NewShipmentInput = {
   originPC: string;
@@ -30,42 +58,90 @@ const validateZipCode = (event: React.ChangeEvent<HTMLInputElement>) => {
 };
 
 const ShipmentForm: FC<Props> = ({ onSubmit }) => {
+  const styles = useStyles();
+
   const { register, handleSubmit } = useForm<NewShipmentInput>();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Typography>Ingrese los datos de el envio para crear envío.</Typography>
-      <TextField
-        label="Codigo postal de origen"
-        {...register("originPC", { required: true })}
-        onChange={validateZipCode}
-      />
-      <TextField
-        label="Codigo postal de destino"
-        {...register("destinationPC", { required: true })}
-        onChange={validateZipCode}
-      />
-      <TextField
-        label="Alto (cm)"
-        type="number"
-        {...register("height", { required: true })}
-        onChange={onlyPositiveNumbers}
-      />
-      <TextField
-        label="Largo (cm)"
-        type="number"
-        {...register("length", { required: true })}
-        onChange={onlyPositiveNumbers}
-      />
-      <TextField
-        label="Ancho (cm)"
-        type="number"
-        {...register("width", { required: true })}
-        onChange={onlyPositiveNumbers}
-      />
-      <Button variant="contained" type="submit">
-        Crear envío
-      </Button>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
+      <Grid container spacing={5} direction="column">
+        <Grid item>
+          <Typography variant="h4" className={styles.title}>
+            Ingrese los datos de el envio.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column" spacing={4}>
+            <Grid item>
+              <Grid container spacing={4}>
+                <Grid item>
+                  <TextField
+                    label="Codigo postal de origen"
+                    {...register("originPC", { required: true })}
+                    onChange={validateZipCode}
+                    variant="outlined"
+                    className={styles.textField}
+                    autoComplete="off"
+                    InputProps={{ className: styles.textField }}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    label="Codigo postal de destino"
+                    {...register("destinationPC", { required: true })}
+                    onChange={validateZipCode}
+                    variant="outlined"
+                    className={styles.textField}
+                    autoComplete="off"
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid container spacing={4}>
+                <Grid item>
+                  <TextField
+                    label="Alto (cm)"
+                    type="number"
+                    {...register("height", { required: true })}
+                    onChange={onlyPositiveNumbers}
+                    variant="outlined"
+                    className={styles.textField}
+                    autoComplete="off"
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    label="Largo (cm)"
+                    type="number"
+                    {...register("length", { required: true })}
+                    onChange={onlyPositiveNumbers}
+                    variant="outlined"
+                    className={styles.textField}
+                    autoComplete="off"
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    label="Ancho (cm)"
+                    type="number"
+                    {...register("width", { required: true })}
+                    onChange={onlyPositiveNumbers}
+                    variant="outlined"
+                    className={styles.textField}
+                    autoComplete="off"
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" type="submit" className={styles.button}>
+            Crear envío
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 };
